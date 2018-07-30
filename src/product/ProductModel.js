dfebf6630c23fb6769e90ab924018d5607ef3a7b
adirokuni <adirokuni@gmail.com>
2018-07-30 21:41:49 +0700

    connect ke mongodb

---
+++ src/product/ProductModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    title: String,
+    author: String,
+    price: Number,
+    stock: Number,
+    image: String
+});
+
+const Product = mongoose.model('products', ProductSchema);
+module.exports = Product;
