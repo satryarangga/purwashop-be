506cba453bb209fb661b4c05a76b472384ad16ef
Satrya Rangga <system@techbro.id>
2018-08-01 20:42:00 +0700

    add to cart

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var mongo = require('./config/mongodb');
 
 var ProductController = require('./src/product/ProductController');
+var CartController = require('./src/cart/CartController');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,5 +12,6 @@ app.use(function(req, res, next) {
 });
 
 app.use('/product', ProductController);
+app.use('/cart', CartController);
 
 module.exports = app;
---
+++ src/cart/CartController.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var mysql = require('../../config/mysql');
+
+router.use( bodyParser.json() );     
+router.use(bodyParser.urlencoded({
+  extended: true
+})); 
+
+router.post('/', function(request, response) {
+	const { body } = request;
+	var data = {
+		customer_id: body.customer_id,
+		product_id: body.product_id,
+		price: body.price,
+		qty: body.qty,
+		subtotal: body.subtotal
+	}
+
+	var sql = "insert into shopping_cart set ?";
+
+	mysql.query(sql, data, (error, result) => {
+		if(error) {
+			response.status(400).send("Something wrong")
+			return
+		}
+
+		response.status(200).send(result);
+	})
+});
+
+module.exports = router;
